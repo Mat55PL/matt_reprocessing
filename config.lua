468ac3d2264e3acbe4802d8937ba1191a311f4cd
Matt <mateusz.pl671@gmail.com>
2019-10-09 21:03:51 +0200

    test commit

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config                            = {}
+
+Config.Item = {
+	marijuana = 400,
+	phone = 250,
+	meth = 420,
+	metyloamina = 250,
+	seed_weed = 125,
+	handcuff = 200,
+	cannabis = 200
+}
+
+Config.Marker = {
+    vector3(460.45, -981.82, 30.69)
+},
