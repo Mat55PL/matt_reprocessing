4a48d57fe9d5e71837c98c9eb94610f431e6aaad
Matt <mateusz.pl671@gmail.com>
2019-10-09 21:15:15 +0200

    add menu

--- client.lua
+++ client.lua
@@ -17,3 +17,35 @@ RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
   PlayerData.job = job
 end)
+
+
+function OpenSoldMenu()
+	ESX.UI.Menu.CloseAll()
+	local elements = {}
+	for k, v in pairs(ESX.GetPlayerData().inventory) do
+		local price = Config.Itemsold[v.name]
+
+		if price and v.count > 0 then
+			table.insert(elements, {
+				label = ('%s - <span style="color:green;">%s</span>'):format(v.label, _U('police_item', ESX.Math.GroupDigits(price))),
+				name = v.name,
+				price = price,
+
+				type = 'slider',
+				value = 1,
+				min = 1,
+				max = v.count
+			})
+		end
+	end
+
+	ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'item_sold', {
+		title    = 'Utylizacja',
+		align    = 'center',
+		elements = elements
+	}, function(data, menu)
+		TriggerServerEvent('matt:sellItem', data.current.name, data.current.value)
+	end, function(data, menu)
+		menu.close()
+	end)
+end	
